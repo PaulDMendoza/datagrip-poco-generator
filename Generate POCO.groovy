3a06752faaea4a0ddf5b3f0147507223f2db5d56
Paul Mendoza <mendoza.paul@gmail.com>
2021-09-14 14:04:17 -0700

    Supports nullable C# features, FKs, same casing
    Some things I wanted to add that I didn't like about the current version. Maybe this should be a new file instead. 
    
    - Added namespace
    - String can be nullable for C#8 support
    - Properties and table names match the case in the database. This is my preference. 
    - Added foreign key properties which is what entity framework likes but only in one direction.

--- Generate POCO.groovy
+++ Generate POCO.groovy
@@ -15,9 +15,11 @@ typeMapping = [
         (~/(?i)^datetimeoffset$/)                         : "DateTimeOffset",
         (~/(?i)^datetime|datetime2|timestamp|date|time$/) : "DateTime",
         (~/(?i)^char$/)                                   : "char",
+        (~/(?i)^text\[\]$/)                                   : "text[]",
+        (~/(?i)^int\[\]$/)                                   : "int[]"
 ]
 
-notNullableTypes = [ "string", "byte[]" ]
+notNullableTypes = [ ]
 
 FILES.chooseDirectoryAndSave("Choose directory", "Choose where to store generated files") { dir ->
     SELECTION.filter { it instanceof DasTable && it.getKind() == ObjectKind.TABLE }.each { generate(it, dir) }
@@ -26,21 +28,38 @@ FILES.chooseDirectoryAndSave("Choose directory", "Choose where to store generate
 def generate(table, dir) {
     def className = csharpName(table.getName())
     def fields = calcFields(table)
-    new File(dir, className + ".cs").withPrintWriter { out -> generate(out, className, fields) }
+    def fks = calcForeignKeys(table)
+    new File(dir, className + ".cs").withPrintWriter { out -> generate(out, className, fields, fks) }
 }
 
-def generate(out, className, fields) {
+def generate(out, className, fields, fks) {
     out.println "using System;"
-    out.println ""
-    out.println "public class $className"
+    out.println "#nullable enable"
+    out.println "namespace PARENTNAMESPACE"
     out.println "{"
+    out.println "   public class $className"
+    out.println "   {"
 
     fields.each() {
-        out.println ""
-        out.println "    public ${it.type} ${it.name} { get; set; }"
+        out.println "      /// <summary>"
+        out.println "      /// "
+        out.println "      /// </summary>"
+        out.println "      /// <example></example>"
+        out.println "      public ${it.type} ${it.name} { get; set; }"
         out.println ""
     }
+
+    fks.each() {
+        out.println "       /// <summary>"
+        out.println "       /// Foreign key to ${it.name} on column(s) ${it.columns}"
+        out.println "       /// </summary>"
+        out.println "       /// <example></example>"
+        out.println "       public ${it.name} ${it.name} { get; set; }"
+        out.println ""
+    }
+    out.println "   }"
     out.println "}"
+    out.println "#nullable restore"
 }
 
 def calcFields(table) {
@@ -54,8 +73,17 @@ def calcFields(table) {
     }
 }
 
+// https://gist.github.com/SoapCanFly/a4d4e5450e2855155fd7ccdf85f61321
+def calcForeignKeys(table) {
+    DasUtil.getForeignKeys(table).reduce([]) {fks, key->
+        fks += [[
+            name: key.getRefTableName(),
+             columns: key.getColumnsRef().names().join(",") ]]
+    }
+}
+
 def csharpName(str) {
     com.intellij.psi.codeStyle.NameUtil.splitNameIntoWords(str)
-            .collect { Case.LOWER.apply(it).capitalize() }
+            .collect { Case.LOWER.apply(it) }
             .join("")
 }
